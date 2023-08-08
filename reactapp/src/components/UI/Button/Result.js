e31464d92e7047d3cee3848457785b66d439ade4
Iamneo <techadmin@iamneo.ai>
2023-08-08 08:04:33 +0000

    Backup Commit

---
+++ reactapp/src/components/UI/Button/Result.js
@@ -0,0 +1,10 @@
+import Button from '../UI/Button/Button';
+function Results({ onResetQuiz }) {
+    return (
+      <div className="results">
+        <Button onClick={onResetQuiz}>Start Quiz</Button>
+      </div>
+    );
+  }
+  
+  export default Results;
